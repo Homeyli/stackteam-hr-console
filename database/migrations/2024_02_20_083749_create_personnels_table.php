1b5162cda31ed992ad3de90930c2c0bd771f2970
Mahdi <homeily@gmail.com>
2024-02-20 12:15:19 +0330

    add personnel model and database

---
+++ database/migrations/2024_02_20_083749_create_personnels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personnels', function (Blueprint $table) {
+            $table->id();
+            $table->integer('devicecode',3);
+            $table->string('firstname',255);
+            $table->string('lastname',255);
+            $table->string('en_name',255);
+            $table->integer('salary',3);
+            $table->bool('is_active',true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personnels');
+    }
+};
