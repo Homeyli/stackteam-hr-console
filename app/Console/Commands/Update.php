d03580abe85bff68b1488cce0ecc9101ac6e62af
Mahdi <homeily@gmail.com>
2024-02-24 11:12:53 +0330

    dev app:update | read personnel id

--- app/Console/Commands/Update.php
+++ app/Console/Commands/Update.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Personnel;
 
 class Update extends Command
 {
@@ -30,14 +31,40 @@ class Update extends Command
         //     [2,'amir','askari']
         // ]);
 
+        // get personnels data ----------------------------------------------
+        $_personnels = Personnel::select(['id','devicecode'])->get();
+        $personnels = [];
+
+        foreach ($_personnels as $key => $value) {
+            $personnels[$value['devicecode']] = $value['id']; 
+        }
+        unset($_personnels);
+        // end get personnels data -----------------------------------------
+
+        // read file -------------------------------------------------------
         $file = file(resource_path('6160062162713_attlog.dat'));
         $data = [];
 
         foreach ($file as $key => $value) {
 
             $record = explode("\t",ltrim($value)); 
-            array_push($data,['device_code' => $record[0],'fingred_at' => $record[1]]); 
+
+            if (!isset($personnels[$record[0]])) {
+
+                if($_p = Personnel::create([
+                    'en_name' => 'Unknow',
+                    'devicecode' => $record[0],
+                    'salary' => '0'
+                ])) {
+
+                    $personnels[$record[0]] = $_p->id;
+                }
+            }
+
+            array_push($data,['personnel_id' => $personnels[$record[0]],'fingred_at' => $record[1]]); 
         }
+
+        // end read file -----------------------------------------------------
 
         print_r($data);
     }
