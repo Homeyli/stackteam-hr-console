fe1b6799c8ace6eacee2eb4fa124914b843903e7
Mahdi <homeily@gmail.com>
2024-03-03 21:35:02 +0330

    + model & mig for timetable

---
+++ database/migrations/2024_03_03_180130_create_time_tables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('time_tables', function (Blueprint $table) {
+            $table->id();
+            $table->integer('year');
+            $table->integer('month');
+            $table->integer('totaldays');
+            $table->integer('fridays');
+            $table->integer('holidays');
+            $table->integer('workdays');
+            $table->integer('totalhours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('time_tables');
+    }
+};
