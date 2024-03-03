f0a0216f1968857b958032234358b913141e7644
mahdi homeyli <homeily@gmail.com>
2024-03-03 12:12:05 +0330

    ++ set user in db > bot

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -5,6 +5,7 @@ namespace App\Telegram\Commands;
 use App\Telegram\CommandInputHelper;
 use Telegram\Bot\Commands\Command;
 
+use App\Models\BotUser;
 
 class StartCommand extends Command
 {
@@ -14,23 +15,46 @@ class StartCommand extends Command
     protected string $name = 'start';
     protected string $description = 'Start Command to get you started';
 
+    private ?BotUser $user = null;
+
+
     public function handle()
     {
-        
-        print_r($this->getUpdate());die();
         $this->replyWithMessage([
-            'text' => "سلام زهرا، به چت بات مدیریت پرسونلی استک تیم خوش آمدی ",
+            'text' => $this->getUser()->username,
+        ]);
+        
+        $this->replyWithMessage([
+            'text' => "سلام :name، به چت بات مدیریت پرسونلی استک تیم خوش آمدی \n\nاز طریق این چت بات میتونید خدمات پرسونلی زیر رو دریافت کنید، برای شروع روی لینک کلیک/تاچ کنید :\n\n1. /timetable جدول موظف کاری\n2. /calcsalary محاسبه حقوق و دستمزد\n3. /workreport ثبت گذارش کار روزانه\n4. /editprofile ویرایش پروفایل پرسونلی",
         ]);
 
-        $nathnalcode = $this->askWithMessage('زهرا جان، لطفا کد ملی خودت رو بنویس');
-        $b = $this->askWithMessage('متولد کجا هستی ؟');
-        $b = $this->askWithMessage("تاریخ تولدت رو با پترن مشابه 23-05-1369 وارد کن ؟");
+        // $nathnalcode = $this->askWithMessage('زهرا جان، لطفا کد ملی خودت رو بنویس');
+        // $b = $this->askWithMessage('متولد کجا هستی ؟');
+        // $b = $this->askWithMessage("تاریخ تولدت رو با پترن مشابه 23-05-1369 وارد کن ؟");
 
-
+        
+        
     }
 
 
     protected function getUser() {
+
+        if (is_null ($this->user)) {
+
+            $this->user = BotUser::updateOrCreate(
+                ['telegram_id' => $this->getUpdate()->getMessage()->from->id],
+                [
+                    // Update or create data
+                    'username' => $this->getUpdate()->getMessage()->from->username,
+                    'first_name' => $this->getUpdate()->getMessage()->from->first_name,
+                    'last_name' => $this->getUpdate()->getMessage()->from->last_name,
+                    'is_bot' => $this->getUpdate()->getMessage()->from->is_bot,
+
+                ]
+            );
+        }
+
+        return $this->user;
 
     }
 
